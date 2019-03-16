7d3978fb90edb49b64df804820137f12125aebd4
Michael Anderson <MacTerrier0@dhcp-v119-031.mobile.uci.edu>
2019-03-16 14:58:54 -0700

    update fetch

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
+    var uluru = { lat: -25.344, lng: 131.036 };
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), { zoom: 4, center: uluru });
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: {lat: -25.344, lng: 131.036}, map: map});
-    var marker2 = new google.maps.Marker({position: {lat: -25.344, lng: 133.036}, map: map});
-    var marker3 = new google.maps.Marker({position: {lat: -25.344, lng: 134.036}, map: map});
-    var marker4 = new google.maps.Marker({position: {lat: -25.344, lng: 135.036}, map: map});
-  }
+    var marker = new google.maps.Marker({ position: { lat: -25.344, lng: 131.036 }, map: map });
+    trackMovement()
+}
+const trackMovement = () => {
+    setInterval(() => {
+        const newLocation = {
+            lat: ~~(Math.random() * 10),
+            lng: ~~(Math.random() * 100)
+        }
+        marker.setPosition(newLocation)
+    }, 2000);
+}
 
+const fetchISSPosition = () => {
+    fetch(`http://api.open-notify.org/iss-now.json`)
+        .then(r => r.json())
+        .then(r => {
+            console.log(r);
+            
+        })
+        .catch(e => {
+            console.error(e)
+        })
+
+}
 // get coordinates of ISS
 // update the marker on the map
 // set interval to update every 10 seconds 
